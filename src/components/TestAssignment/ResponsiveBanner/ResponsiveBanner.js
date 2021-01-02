4329049170bc5e24339f1ac4ec679eb2de8ad399
MelnikVitali <mvitalim@gmail.com>
2021-01-03 00:03:55 +0200

    add webp banner

--- src/components/TestAssignment/ResponsiveBanner/ResponsiveBanner.js
+++ src/components/TestAssignment/ResponsiveBanner/ResponsiveBanner.js
@@ -13,6 +13,19 @@ import bannerImage1024 from '../../../assets/bannerPhoto/banner-photo1024.jpg';
 import bannerImage1024_2x from '../../../assets/bannerPhoto/banner-photo1024@2x.jpg';
 import bannerImage1024_3x from '../../../assets/bannerPhoto/banner-photo1024@3x.jpg';
 
+import bannerWebpImage360 from '../../../assets/bannerPhoto/banner-photo360.webp';
+import bannerWebpImage360_2x from '../../../assets/bannerPhoto/banner-photo360@2x.webp';
+import bannerWebpImage360_3x from '../../../assets/bannerPhoto/banner-photo360@3x.webp';
+import bannerWebpImage526 from '../../../assets/bannerPhoto/banner-photo526.webp';
+import bannerWebpImage526_2x from '../../../assets/bannerPhoto/banner-photo526@2x.webp';
+import bannerWebpImage526_3x from '../../../assets/bannerPhoto/banner-photo526@3x.webp';
+import bannerWebpImage768 from '../../../assets/bannerPhoto/banner-photo768.webp';
+import bannerWebpImage768_2x from '../../../assets/bannerPhoto/banner-photo768@2x.webp';
+import bannerWebpImage768_3x from '../../../assets/bannerPhoto/banner-photo768@3x.webp';
+import bannerWebpImage1024 from '../../../assets/bannerPhoto/banner-photo1024.webp';
+import bannerWebpImage1024_2x from '../../../assets/bannerPhoto/banner-photo1024@2x.webp';
+import bannerWebpImage1024_3x from '../../../assets/bannerPhoto/banner-photo1024@3x.webp';
+
 
 import useStyles from './styles';
 
@@ -25,10 +38,29 @@ const ResponsiveBanner = React.memo(() => {
                 <source
                     media='(max-width: 480px)'
                     srcSet={
+                        `${bannerWebpImage360} 1x,
+                         ${bannerWebpImage360_2x} 2x,
+                         ${bannerWebpImage360_3x} 3x`
+                    }
+                    type="image/webp"
+                />
+                <source
+                    media='(max-width: 480px)'
+                    srcSet={
                         `${bannerImage360} 1x,
                          ${bannerImage360_2x} 2x,
                          ${bannerImage360_3x} 3x`
                     }
+                />
+
+                <source
+                    media='(max-width: 767px)'
+                    srcSet={
+                        `${bannerWebpImage526} 1x,
+                         ${bannerWebpImage526_2x} 2x,
+                         ${bannerWebpImage526_3x} 3x`
+                    }
+                    type="image/webp"
                 />
                 <source
                     media='(max-width: 767px)'
@@ -38,6 +70,17 @@ const ResponsiveBanner = React.memo(() => {
                          ${bannerImage526_3x} 3x`
                     }
                 />
+
+
+                <source
+                    media='(max-width: 1023px)'
+                    srcSet={
+                        `${bannerWebpImage768} 1x,
+                         ${bannerWebpImage768_2x} 2x,
+                         ${bannerWebpImage768_3x} 3x`
+                    }
+                    type="image/webp"
+                />
                 <source
                     media='(max-width: 1023px)'
                     srcSet={
@@ -46,14 +89,25 @@ const ResponsiveBanner = React.memo(() => {
                          ${bannerImage768_3x} 3x`
                     }
                 />
+
                 <source
-                    media='(max-width: 3064px)'
+                    media='(max-width: 2560px)'
+                    srcSet={
+                        `${bannerWebpImage1024} 1x,
+                         ${bannerWebpImage1024_2x} 2x,
+                         ${bannerWebpImage1024_3x} 3x`
+                    }
+                    type="image/webp"
+                />
+                <source
+                    media='(max-width: 2560px)'
                     srcSet={
                         `${bannerImage1024} 1x,
                          ${bannerImage1024_2x} 2x,
                          ${bannerImage1024_3x} 3x`
                     }
                 />
+
                 <img
                     src={bannerImage1024}
                     alt="Man with laptop in headphones"
