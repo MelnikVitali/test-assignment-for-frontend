bcaa41b790f79fb1d705f8d105d58711288ae542
MelnikVitali <mvitalim@gmail.com>
2021-01-09 23:45:47 +0200

    Updates

---
+++ precache-manifest.67a62a5cef29e1a620473dbf09296024.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bba5aa903e35a44e70262968b6a92e4",
+    "url": "/test-assignment-for-frontend/index.html"
+  },
+  {
+    "revision": "6a10532550ca7dca0822",
+    "url": "/test-assignment-for-frontend/static/css/2.812b49b4.chunk.css"
+  },
+  {
+    "revision": "6a10532550ca7dca0822",
+    "url": "/test-assignment-for-frontend/static/js/2.127bb349.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/test-assignment-for-frontend/static/js/2.127bb349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2b7051f40fbf674def",
+    "url": "/test-assignment-for-frontend/static/js/3.c6d59da9.chunk.js"
+  },
+  {
+    "revision": "db4da78ecb163b8f4478",
+    "url": "/test-assignment-for-frontend/static/js/main.379c5970.chunk.js"
+  },
+  {
+    "revision": "a4dfe8b0536be44655c1",
+    "url": "/test-assignment-for-frontend/static/js/runtime-main.704e72af.js"
+  },
+  {
+    "revision": "a33f2d3cee0ddafd303461d319e9a2ab",
+    "url": "/test-assignment-for-frontend/static/media/avatar.a33f2d3c.svg"
+  },
+  {
+    "revision": "4702abf1ae6013e641ef083945182218",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo1024.4702abf1.webp"
+  },
+  {
+    "revision": "69e99028fb3aff0756cb911ac9efcef3",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo1024.69e99028.jpg"
+  },
+  {
+    "revision": "6e0158b73fb28ffb51c376b8912d28a1",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo1024@2x.6e0158b7.webp"
+  },
+  {
+    "revision": "9543dfde02b1e508f418a0a0a7be1b7d",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo1024@2x.9543dfde.jpg"
+  },
+  {
+    "revision": "5fa1c847bd0dc43313a6c134b1689bf2",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo1024@3x.5fa1c847.jpg"
+  },
+  {
+    "revision": "f2a83c0e7290bb538e8058d593709e30",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo1024@3x.f2a83c0e.webp"
+  },
+  {
+    "revision": "cd0265bd00c62e8cd5ec94bbc771d70b",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo360.cd0265bd.jpg"
+  },
+  {
+    "revision": "fd67d2602779388d04c8da528ca9e173",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo360.fd67d260.webp"
+  },
+  {
+    "revision": "7d71bf300427ff65b8b24ecdbe964bc3",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo360@2x.7d71bf30.webp"
+  },
+  {
+    "revision": "fdb437e6f8d7e4f91d539de26d3beae1",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo360@2x.fdb437e6.jpg"
+  },
+  {
+    "revision": "1109cc2e945793d365f03c268f0a3e01",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo360@3x.1109cc2e.webp"
+  },
+  {
+    "revision": "9f658601ae229574f38118075406391a",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo360@3x.9f658601.jpg"
+  },
+  {
+    "revision": "9233c4b3da6d6061474aa45d1ad6c8af",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo526.9233c4b3.jpg"
+  },
+  {
+    "revision": "bf44812b9954ac9d3e89b977fe4fcbcc",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo526.bf44812b.webp"
+  },
+  {
+    "revision": "8ab047c6b1b3fd8e2c7decfe18562934",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo526@2x.8ab047c6.jpg"
+  },
+  {
+    "revision": "e3444ea91e76373f8ea089a17f508b99",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo526@2x.e3444ea9.webp"
+  },
+  {
+    "revision": "078bdae52767f7adef0c8eb26e3a1b4b",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo526@3x.078bdae5.webp"
+  },
+  {
+    "revision": "43ca95626151cbec771ab33808b74990",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo526@3x.43ca9562.jpg"
+  },
+  {
+    "revision": "763d5af26e182f671aefd5b8ebb97f3b",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo768.763d5af2.webp"
+  },
+  {
+    "revision": "9511718cbb72d542e5ee9b7ea28a991b",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo768.9511718c.jpg"
+  },
+  {
+    "revision": "6088de7485e8367fdba9bc73b03c9920",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo768@2x.6088de74.jpg"
+  },
+  {
+    "revision": "ea25be97d938c0bbb79304d480b77397",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo768@2x.ea25be97.webp"
+  },
+  {
+    "revision": "80567977b4a3bcae5aa9e687e4baf32a",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo768@3x.80567977.jpg"
+  },
+  {
+    "revision": "f19a6ae35f9c663f599f323621070ae8",
+    "url": "/test-assignment-for-frontend/static/media/banner-photo768@3x.f19a6ae3.webp"
+  },
+  {
+    "revision": "bce28fb772838de34344b20fa26296b0",
+    "url": "/test-assignment-for-frontend/static/media/logo.bce28fb7.svg"
+  },
+  {
+    "revision": "f28cb247d5778a3e9e6e5be021b9a81b",
+    "url": "/test-assignment-for-frontend/static/media/man-laptop-v1.f28cb247.svg"
+  },
+  {
+    "revision": "63637e948daaff9637f3f5a803d2b669",
+    "url": "/test-assignment-for-frontend/static/media/menu-icon.63637e94.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.465df98f8bcbf8af860ed7cab7fc5f0c.js"
+  "/test-assignment-for-frontend/precache-manifest.67a62a5cef29e1a620473dbf09296024.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-assignment-for-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
